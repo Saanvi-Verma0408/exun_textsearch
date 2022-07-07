8936e38c6fdd3a756e994a5062a67c76f4885ae3
Saanvi Verma <106982741+Saanvi-Verma0408@users.noreply.github.com>
2022-07-07 21:53:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const input = process.argv[2];
-
 const word = input.split(' ');
-fs.readFile('./example.txt', 'utf8', (err, data) => {
+
+fs.readFile('./text.txt', 'utf8', (err, data) => {
     if (err) throw err;
     console.log(data.includes(word));
 });
